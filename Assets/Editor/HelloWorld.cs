604d723cfd75d9b173801a7ecab2eb9247771c23
Mahmoud Elmadah <mahmoud.sayed235@gmail.com>
2020-10-30 19:23:35 +0200

    create new GameObject with new properties using Extending Unity Editor

---
+++ Assets/Editor/HelloWorld.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class HelloWorld : EditorWindow
+{
+    [MenuItem("GameObject/Hello World")]
+    private static void CreateHelloWorldGameObject()
+    {
+        if (EditorUtility.DisplayDialog(
+            "Hello World",
+            "Do you really want to do this?",
+            "Create",
+            "Cancel"))
+        {
+            GameObject helloWorld= new GameObject("HelloWorld");
+            //set tag to the game object
+            helloWorld.tag = "HelloWorld";
+            //add any component to the GameObject
+            helloWorld.AddComponent<Rigidbody>();
+        }
+    }
+}
